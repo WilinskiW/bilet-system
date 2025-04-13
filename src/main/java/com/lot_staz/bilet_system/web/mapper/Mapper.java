b89f75f559ccddd894e7d116a48dce668e8250c5
Wiktor Wilinski <wilinskiwiktor2005@gmail.com>
2025-04-13 11:18:21 +0200

    Docs of mapper interface

--- src/main/java/com/lot_staz/bilet_system/web/mapper/Mapper.java
+++ src/main/java/com/lot_staz/bilet_system/web/mapper/Mapper.java
@@ -3,6 +3,13 @@ package com.lot_staz.bilet_system.web.mapper;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Generic interface for mapping between database entities (TEntity) and their corresponding Data Transfer Objects (TDto).
+ * Supports conversion of single objects as well as lists.
+ *
+ * @param <TEntity> the type of the entity (JPA entity)
+ * @param <TDto> the type of the DTO (Data Transfer Object)
+ */
 public interface Mapper<TEntity, TDto> {
     TDto entityToDto(TEntity entity);
 

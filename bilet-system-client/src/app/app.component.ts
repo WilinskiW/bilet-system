5e198d2b9927b84c56e582b69923295c8f7a9ef6
Wiktor Wilinski <wilinskiwiktor2005@gmail.com>
2025-04-13 19:23:03 +0200

    sidebar template

--- bilet-system-client/src/app/app.component.ts
+++ bilet-system-client/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    RouterOutlet,
+    SidebarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'bilet-system-client';
-}
+export class AppComponent { }

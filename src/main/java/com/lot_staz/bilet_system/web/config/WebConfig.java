ce494355336dfa6ae4e9a3cbfb49f7de178afdbb
Wiktor Wilinski <wilinskiwiktor2005@gmail.com>
2025-04-15 23:12:10 +0200

    change allowedOrigin to allowedOriginPatterns

--- src/main/java/com/lot_staz/bilet_system/web/config/WebConfig.java
+++ src/main/java/com/lot_staz/bilet_system/web/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200", "https://wilinskiw.github.io")
+                        .allowedOriginPatterns("http://localhost:4200", "https://wilinskiw.github.io")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
